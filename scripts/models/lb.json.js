69d02add727c8e035b7bb35deed3248226c30875
bobby <bbahov@yahoo.com>
2015-02-06 15:27:24 +0100

    Tweak on initial design

---
+++ scripts/models/lb.json.js
@@ -0,0 +1,14 @@
+/*jslint browser: true, devel: true*/
+/*globals angular, _*/
+/**
+ * Model for bord tiles
+ */
+(function () {
+    angular.module('lbApp')
+        .factory('lbModel', function () {
+            var lb = {
+
+            };
+            return lb;
+        });
+})();
---
+++ scripts/models/user.json.js
@@ -0,0 +1,16 @@
+/*jslint browser: true, devel: true*/
+/*globals angular, _*/
+/**
+ * Model for bord tiles
+ */
+(function () {
+    angular.module('lbApp')
+        .factory('userdModel', function () {
+            var user = {
+
+            }
+
+
+            return user;
+        });
+})();
